41cf237805a51b60f04ecf91bafb2db2a81a6061
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-05-21 10:46:33 +0300

    Added ore vein feature generation logic

---
+++ src/main/java/ru/jtcf/undermined/worldgen/OreVein.java
@@ -0,0 +1,6 @@
+package ru.jtcf.undermined.worldgen;
+
+import net.minecraft.resources.ResourceLocation;
+
+public record OreVein(int value, ResourceLocation resource) {
+}
---
+++ src/main/java/ru/jtcf/undermined/worldgen/OreVeinManager.java
@@ -0,0 +1,70 @@
+package ru.jtcf.undermined.worldgen;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.storage.DimensionDataStorage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OreVeinManager extends SavedData {
+    private final Map<ChunkPos, OreVein> veinMap = new HashMap<>();
+
+    public static OreVeinManager get(Level level) {
+        if (level.isClientSide()) {
+            throw new RuntimeException("Access on client-side is not allowed!");
+        }
+
+        DimensionDataStorage storage = ((ServerLevel)level).getDataStorage();
+
+        return storage.computeIfAbsent(OreVeinManager::new, OreVeinManager::new, "ore_vein_manager");
+    }
+
+    @Nullable
+    public OreVein getVein(long seed, ChunkPos pos, OreVeinFeatureConfiguration config) {
+        return veinMap.computeIfAbsent(pos, cp -> {
+            int value = config.getValuesForChunk(seed, cp.x, cp.z);
+            if (value > 0) {
+                return new OreVein(value, config.resource());
+            }
+            return null;
+        });
+    }
+
+    public OreVeinManager() {}
+
+    public OreVeinManager(CompoundTag nbt) {
+        ListTag veins = nbt.getList("veins", Tag.TAG_COMPOUND);
+        for (Tag t : veins) {
+            CompoundTag vein = (CompoundTag) t;
+            int veinValue = vein.getInt("value");
+            ResourceLocation resource = new ResourceLocation(vein.getString("resource"));
+            ChunkPos pos = new ChunkPos(vein.getInt("x"), vein.getInt("z"));
+            veinMap.put(pos, new OreVein(veinValue, resource));
+        }
+    }
+
+    @Override
+    @NotNull
+    public CompoundTag save(CompoundTag pCompoundTag) {
+        ListTag veins = new ListTag();
+        veinMap.forEach((pos, vein) -> {
+            CompoundTag veinTag = new CompoundTag();
+            veinTag.putInt("x", pos.x);
+            veinTag.putInt("z", pos.z);
+            veinTag.putInt("value", vein.value());
+            veinTag.putString("resource", vein.resource().toString());
+            veins.add(veinTag);
+        });
+        pCompoundTag.put("veins", veins);
+        return pCompoundTag;
+    }
+}
