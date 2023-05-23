3ff95a964e2c5f5fe3d300cbd858ec2613c43029
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-05-23 16:33:04 +0300

    Robustify ResourceVeinManager

--- src/main/java/ru/jtcf/undermined/worldgen/ResourceVeinManager.java
+++ src/main/java/ru/jtcf/undermined/worldgen/ResourceVeinManager.java
@@ -1,12 +1,12 @@
 package ru.jtcf.undermined.worldgen;
 
+import com.mojang.logging.LogUtils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.storage.DimensionDataStorage;
@@ -15,13 +15,16 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 import ru.jtcf.undermined.setup.Registration;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ResourceVeinManager extends SavedData {
+    private static final Logger LOGGER = LogUtils.getLogger();
     private final Map<ChunkPos, ResourceVein> veinMap = new HashMap<>();
 
     public static ResourceVeinManager get(LevelAccessor level) {
@@ -39,12 +42,19 @@ public class ResourceVeinManager extends SavedData {
         if (event.getWorld().isClientSide()) {
             return;
         }
-        ServerLevel level = (ServerLevel)event.getWorld();
+        ServerLevel level = (ServerLevel) event.getWorld();
         ResourceVeinManager veinManager = ResourceVeinManager.get(event.getWorld());
         ChunkPos chunk = event.getChunk().getPos();
         ResourceLocation dimension = level.dimension().location();
-        ResourceVeinPlacementConfiguration config =
-                Registration.RESOURCE_VEIN_PLACEMENTS_REGISTRY_SUPPLIER.get().getValue(dimension);
+        Optional<ResourceVeinPlacementConfiguration> configOpt
+                = Registration.RESOURCE_VEIN_PLACEMENTS_REGISTRY_SUPPLIER.get().getValues()
+                .stream()
+                .filter((cfg) -> cfg.dimension().compareTo(dimension) == 0)
+                .findFirst();
+        if (configOpt.isEmpty()) {
+            return;
+        }
+        ResourceVeinPlacementConfiguration config = configOpt.get();
         long seed = level.getSeed();
         veinManager.getVein(seed, chunk, config);
     }
@@ -54,7 +64,7 @@ public class ResourceVeinManager extends SavedData {
         return veinMap.computeIfAbsent(pos, cp -> {
             if (config != null) {
                 Collection<ResourceVeinResourceConfiguration> resourceConfigs =
-                Registration.RESOURCE_VEINS_REGISTRY_SUPPLIER.get().getValues();
+                        Registration.RESOURCE_VEINS_REGISTRY_SUPPLIER.get().getValues();
                 ResourceVein vein = config.getVeinForChunk(seed, cp, resourceConfigs);
                 if (vein.value() > 0) {
                     return vein;
