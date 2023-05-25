459c2000c7429a4671dbfb6ca66e248634a13396
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-05-25 20:41:45 +0300

    Cleaned up stuff left from the other project

--- src/main/java/ru/jtcf/undermined/data/client/ModBlockStateProvider.java
+++ src/main/java/ru/jtcf/undermined/data/client/ModBlockStateProvider.java
@@ -3,9 +3,7 @@ package ru.jtcf.undermined.data.client;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import ru.jtcf.undermined.UnderMined;
 
@@ -16,88 +14,7 @@ public class ModBlockStateProvider extends BlockStateProvider {
         super(gen, UnderMined.MODID, existingFileHelper);
     }
 
-    private static class RotationPair {
-        int x;
-        int y;
-
-        public RotationPair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    private static final Map<Direction, RotationPair> DIRECTION_ROTATIONS =
-            ImmutableMap.<Direction, RotationPair>builder()
-                    .put(Direction.NORTH, new RotationPair(0, 0))
-                    .put(Direction.SOUTH, new RotationPair(0, 180))
-                    .put(Direction.EAST, new RotationPair(0, 90))
-                    .put(Direction.WEST, new RotationPair(0, 270))
-                    .put(Direction.DOWN, new RotationPair(90, 0))
-                    .put(Direction.UP, new RotationPair(270, 0))
-                    .build();
-
-    /*
-    public void registerPipeRegular() {
-        MultiPartBlockStateBuilder builder =
-                getMultipartBuilder(ModBlocks.PIPE_REGULAR.get()).part().modelFile(models().getExistingFile(modLoc(
-                        "block/pipe_regular_frame"))).addModel().end();
-        for (var dir : Direction.values()) {
-            builder = builder
-                    .part()
-                    .modelFile(models().getExistingFile(modLoc("block/pipe_regular_frame_cover")))
-                    .rotationX(DIRECTION_ROTATIONS.get(dir).x)
-                    .rotationY(DIRECTION_ROTATIONS.get(dir).y)
-                    .addModel()
-                    .condition(EnumProperty.create(dir.getName(), ConnectionType.class), ConnectionType.DISCONNECTED)
-                    .end()
-                    .part()
-                    .modelFile(models().getExistingFile(modLoc("block/pipe_regular_junction")))
-                    .rotationX(DIRECTION_ROTATIONS.get(dir).x)
-                    .rotationY(DIRECTION_ROTATIONS.get(dir).y)
-                    .addModel()
-                    .condition(EnumProperty.create(dir.getName(), ConnectionType.class), ConnectionType.CONNECTED)
-                    .end();
-        }
-
-    }
-    */
-
-    /*
-    public void registerPipeSmart() {
-        MultiPartBlockStateBuilder builder =
-                getMultipartBuilder(ModBlocks.PIPE_SMART.get()).part().modelFile(models().getExistingFile(modLoc(
-                        "block/pipe_smart_core"))).addModel().end();
-        for (var dir : Direction.values()) {
-            builder = builder
-                    .part()
-                    .modelFile(models().getExistingFile(modLoc("block/pipe_smart_frame_cover")))
-                    .rotationX(DIRECTION_ROTATIONS.get(dir).x)
-                    .rotationY(DIRECTION_ROTATIONS.get(dir).y)
-                    .addModel()
-                    .condition(EnumProperty.create(dir.getName(), ConnectionType.class), ConnectionType.DISCONNECTED)
-                    .end()
-                    .part()
-                    .modelFile(models().getExistingFile(modLoc("block/pipe_regular_junction")))
-                    .rotationX(DIRECTION_ROTATIONS.get(dir).x)
-                    .rotationY(DIRECTION_ROTATIONS.get(dir).y)
-                    .addModel()
-                    .condition(EnumProperty.create(dir.getName(), ConnectionType.class), ConnectionType.CONNECTED)
-                    .end()
-                    .part()
-                    .modelFile(models().getExistingFile(modLoc("block/pipe_smart_junction")))
-                    .rotationX(DIRECTION_ROTATIONS.get(dir).x)
-                    .rotationY(DIRECTION_ROTATIONS.get(dir).y)
-                    .addModel()
-                    .condition(EnumProperty.create(dir.getName(), ConnectionType.class), ConnectionType.CONNECTED_SMART)
-                    .end();
-        }
-    }
-    */
-
     @Override
     protected void registerStatesAndModels() {
-        //registerPipeRegular();
-        //registerPipeSmart();
-        //horizontalBlock(ModBlocks.NETWORK_INTERFACE.get(), models().getExistingFile(modLoc("block/network_interface")));
     }
 }
--- src/main/java/ru/jtcf/undermined/data/client/ModItemModelProvider.java
+++ src/main/java/ru/jtcf/undermined/data/client/ModItemModelProvider.java
@@ -8,26 +8,13 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import ru.jtcf.undermined.UnderMined;
 
 public class ModItemModelProvider extends ItemModelProvider {
-    private static String[] MODULES = {"admin_module", "blank_module", "crafting_module", "crafting_observer_module",
-            "crafting_satellite_module", "energy_booster_module", "energy_extractor_module", "energy_provider_module",
-            "extraction_amount_module", "extraction_speed_module", "fluid_provider_module", "fluid_receiver_module",
-            "item_provider_module", "item_receiver_module", "network_map_module"};
-
     public ModItemModelProvider(DataGenerator gen, ExistingFileHelper existingFileHelper) {
         super(gen, UnderMined.MODID, existingFileHelper);
     }
 
     @Override
     protected void registerModels() {
-        //withExistingParent("pipe_regular", modLoc("block/pipe_regular_frame"));
-        //withExistingParent("pipe_smart", modLoc("block/pipe_smart_core"));
-        //withExistingParent("network_interface", modLoc("block/network_interface"));
-
         ModelFile itemGenerated = getExistingFile(mcLoc("item/generated"));
-
-        //for (String module_name : MODULES) {
-            //generatedBuilder(itemGenerated, module_name);
-        //}
     }
 
     private ItemModelBuilder generatedBuilder(ModelFile itemGenerated, String name) {
