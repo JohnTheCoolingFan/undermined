c3c12d3e59b87f42527566611994492de723b3c3
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-05-26 14:19:59 +0300

    Imports cleanup

--- src/main/java/ru/jtcf/undermined/data/client/ModBlockStateProvider.java
+++ src/main/java/ru/jtcf/undermined/data/client/ModBlockStateProvider.java
@@ -1,13 +1,9 @@
 package ru.jtcf.undermined.data.client;
 
-import com.google.common.collect.ImmutableMap;
-import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import ru.jtcf.undermined.UnderMined;
-
-import java.util.Map;
 
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper existingFileHelper) {
